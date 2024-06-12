48a3787878dd31a92a3e141f951d6f6ecf05f97f
dicethedev <dicethedev@gmail.com>
2024-06-12 17:03:42 +0000

    Sweep setting

---
+++ src/constants/theme.ts
@@ -0,0 +1,13 @@
+export const COLORS = {
+  primary: "#FFE455",
+  primaryLight: "#E8D4B092",
+  primary2: "#B1BBCD",
+  black: "#000000",
+  dark: "#1a1e26",
+  night: "#14161a",
+  btnGradient: "radial-gradient(50% 50% at 50% 50%, #0099FB 0%, #006DED 100%)",
+  borderColor: "#E7BFE7",
+  inputBgcolor: "#B5B4C6",
+  balTextColor: "#006DED",
+  tabTextColor: "#9E829F",
+};
