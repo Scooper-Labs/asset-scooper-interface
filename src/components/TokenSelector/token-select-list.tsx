43f4bbbeb2d6b70cafce9cc6ec60d61742dc3e35
Zenon <sunday.m1701072@st.futminna.edu.ng>
2024-06-14 13:54:51 +0000

    Tokenselect in progress... todo, token row, select functionality with redux

--- src/components/TokenSelector/token-select-list.tsx
+++ src/components/TokenSelector/token-select-list.tsx
@@ -1,11 +1,46 @@
 import { useTokenLists } from "@/hooks/useTokens";
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, VStack } from "@chakra-ui/react";
+import React, { useEffect, useRef, useState } from "react";
+import tokenList from "@/constants/baseTokenList.json";
+import TokenSelectListRow from "./token-select-row";
 
 function TokenSelectList() {
-  const { data } = useTokenLists({ chainId: 8453 });
-  console.log("tokenList", data);
-  return <Box height="370px">Hello World</Box>;
+  console.log("token list json", tokenList);
+  const vStackRef = useRef<HTMLDivElement | null>(null);
+  const [_height, setHeight] = useState(0);
+  const handleHeightCheck = () => {
+    if (vStackRef.current) {
+      const height = vStackRef.current.offsetHeight; // Access the height
+      console.log("VStack height:", height);
+      setHeight(height);
+    } else {
+      console.log("VStack ref not yet available");
+    }
+  };
+
+  useEffect(() => {
+    handleHeightCheck();
+  }, [vStackRef]);
+
+  return (
+    <VStack
+      borderTop="1px solid #F7E5F7"
+      width="100%"
+      height="341px"
+      padding="1rem"
+      overflowY="scroll"
+      ref={vStackRef}
+    >
+      {/* <Button onClick={handleHeightCheck}>{_height ? _height : "e"}</Button> */}
+      {tokenList.slice(0, 10).map((token, i) => {
+        return (
+          <Box key={i}>
+            <TokenSelectListRow token={token} />
+          </Box>
+        );
+      })}
+    </VStack>
+  );
 }
 
 export default TokenSelectList;
--- src/components/TokenSelector/token-select-row.tsx
+++ src/components/TokenSelector/token-select-row.tsx
@@ -1,7 +1,27 @@
+import { Token } from "@/lib/components/types";
+import { Box, Checkbox, HStack, Text, VStack } from "@chakra-ui/react";
+import Image from "next/image";
 import React from "react";
 
-function TokenSelectList() {
-  return <div>Hello World</div>;
+function TokenSelectListRow({ token }: { token: Token }) {
+  const { address, logoURI, name, symbol, decimals } = token;
+  return (
+    <HStack>
+      <Checkbox colorScheme="red" >
+        <HStack>
+          <Box overflow="hidden" rounded="100%">
+            <Image src={logoURI} width={20} height={20} alt={name} />
+          </Box>
+          <VStack>
+            <Text fontWeight="700">{symbol}</Text>
+            <Text color="#A8BBD6" fontSize="smaller">
+              {name}
+            </Text>
+          </VStack>
+        </HStack>
+      </Checkbox>
+    </HStack>
+  );
 }
 
-export default TokenSelectList;
+export default TokenSelectListRow;
