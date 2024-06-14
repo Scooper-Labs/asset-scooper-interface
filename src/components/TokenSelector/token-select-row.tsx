d312a1110a9a1a8a39d6e979f60adc5a54bc3471
Zenon <sunday.m1701072@st.futminna.edu.ng>
2024-06-14 14:55:10 +0000

    Token selector row ui completed.. todos: redux state for sweep, balance, price

--- src/components/TokenSelector/token-select-row.tsx
+++ src/components/TokenSelector/token-select-row.tsx
@@ -3,16 +3,21 @@ import { Box, Checkbox, HStack, Text, VStack } from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
 
+export interface TokenInfo extends Token {
+  price: number;
+  amount: string;
+}
+
 function TokenSelectListRow({ token }: { token: Token }) {
   const { address, logoURI, name, symbol, decimals } = token;
   return (
-    <HStack>
-      <Checkbox colorScheme="red" >
-        <HStack>
+    <HStack justifyContent="space-between" width="100%" whiteSpace="nowrap">
+      <Checkbox colorScheme="#fff" iconColor="#7F56D9">
+        <HStack alignItems="center">
           <Box overflow="hidden" rounded="100%">
-            <Image src={logoURI} width={20} height={20} alt={name} />
+            <Image src={logoURI} width={25} height={25} alt={name} />
           </Box>
-          <VStack>
+          <VStack gap="0" alignItems="start">
             <Text fontWeight="700">{symbol}</Text>
             <Text color="#A8BBD6" fontSize="smaller">
               {name}
@@ -20,6 +25,15 @@ function TokenSelectListRow({ token }: { token: Token }) {
           </VStack>
         </HStack>
       </Checkbox>
+
+      {/* bALANCE AND price panels*/}
+
+      <VStack alignItems="end" gap="0">
+        <Text fontWeight="700">0.002 {symbol}</Text>
+        <Text color="#A8BBD6" fontSize="smaller">
+          ~$12.5
+        </Text>
+      </VStack>
     </HStack>
   );
 }
