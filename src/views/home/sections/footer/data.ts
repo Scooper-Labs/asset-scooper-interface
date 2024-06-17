e863de8f7e5e0eb17bfadad0eefa4d25493d1157
dicethedev <dicethedev@gmail.com>
2024-06-17 18:38:21 +0000

    add github URL

--- src/views/home/sections/footer/data.ts
+++ src/views/home/sections/footer/data.ts
@@ -5,7 +5,11 @@ import { BiLogoTelegram } from "react-icons/bi";
 
 const scooperSocialLinks = [
   { icon: FaXTwitter, url: "https://x.com/assetscooper", ariaLabel: "X" },
-  { icon: FaGithub, url: "https://github.com/yourgithub", ariaLabel: "GitHub" },
+  {
+    icon: FaGithub,
+    url: "https://github.com/scooper-labs",
+    ariaLabel: "GitHub",
+  },
   {
     icon: BiLogoTelegram,
     url: "https://t.me/yourtelegram",
