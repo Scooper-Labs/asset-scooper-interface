653a7b6f9ff66bef76a4bf3b1343a1e2b7e37b2c
dicethedev <dicethedev@gmail.com>
2024-06-17 18:36:09 +0000

    update-[landing page UI]

---
+++ src/views/home/sections/footer/data.ts
@@ -0,0 +1,21 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { SiFarcaster } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { BiLogoTelegram } from "react-icons/bi";
+
+const scooperSocialLinks = [
+  { icon: FaXTwitter, url: "https://x.com/assetscooper", ariaLabel: "X" },
+  { icon: FaGithub, url: "https://github.com/yourgithub", ariaLabel: "GitHub" },
+  {
+    icon: BiLogoTelegram,
+    url: "https://t.me/yourtelegram",
+    ariaLabel: "Telegram",
+  },
+  {
+    icon: SiFarcaster,
+    url: "https://farcaster.com/yourfarcaster",
+    ariaLabel: "Farcaster",
+  },
+];
+
+export { scooperSocialLinks };
