7d28fda2792c585321f25f84a7b40a08546761d1
Zenon <sunday.m1701072@st.futminna.edu.ng>
2024-06-18 04:51:10 +0000

    covalent sdk installed, token balance

--- src/components/TokenSelector/token-select-list.tsx
+++ src/components/TokenSelector/token-select-list.tsx
@@ -3,25 +3,35 @@ import { Box, Button, VStack } from "@chakra-ui/react";
 import React, { useEffect, useRef, useState } from "react";
 import tokenList from "@/constants/baseTokenList.json";
 import TokenSelectListRow from "./token-select-row";
+import { useBalances } from "@/hooks/balances/useBalances";
+import { useAccount } from "wagmi";
 
 function TokenSelectList() {
-  console.log("token list json", tokenList);
-  const vStackRef = useRef<HTMLDivElement | null>(null);
-  const [_height, setHeight] = useState(0);
-  const handleHeightCheck = () => {
-    if (vStackRef.current) {
-      const height = vStackRef.current.offsetHeight; // Access the height
-      console.log("VStack height:", height);
-      setHeight(height);
-    } else {
-      console.log("VStack ref not yet available");
-    }
-  };
+  // console.log("token list json", tokenList);
+  // const vStackRef = useRef<HTMLDivElement | null>(null);
+  // const [_height, setHeight] = useState(0);
+  // const handleHeightCheck = () => {
+  //   if (vStackRef.current) {
+  //     const height = vStackRef.current.offsetHeight; // Access the height
+  //     console.log("VStack height:", height);
+  //     setHeight(height);
+  //   } else {
+  //     console.log("VStack ref not yet available");
+  //   }
+  // };
 
-  useEffect(() => {
-    handleHeightCheck();
-  }, [vStackRef]);
+  // useEffect(() => {
+  //   handleHeightCheck();
+  // }, [vStackRef]);
 
+  const { address } = useAccount();
+  const addressMap = tokenList.map((token) => token.address);
+
+  const { data, isFetching, isLoading } = useBalances({
+    tokens: tokenList,
+    account: address ?? "",
+  });
+  console.log("this is balance data", data, isFetching, isLoading);
   return (
     <VStack
       borderTop="1px solid #F7E5F7"
@@ -29,13 +39,13 @@ function TokenSelectList() {
       height="341px"
       padding="1rem"
       overflowY="scroll"
-      ref={vStackRef}
+      // ref={vStackRef}
       gap="12px"
     >
       {/* <Button onClick={handleHeightCheck}>{_height ? _height : "e"}</Button> */}
-      {tokenList.slice(0, 10).map((token, i) => {
+      {tokenList.map((token, i) => {
         return (
-          <Box key={i}  width="100%">
+          <Box key={i} width="100%">
             <TokenSelectListRow token={token} />
           </Box>
         );
