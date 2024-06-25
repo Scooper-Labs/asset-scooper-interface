820066c2b665808de93f26149edfa3c074db0db5
dicethedev <dicethedev@gmail.com>
2024-06-25 12:32:13 +0100

    update-[fix landing page UI bugs]

--- src/views/home/navbar/appAction.tsx
+++ src/views/home/navbar/appAction.tsx
@@ -2,18 +2,17 @@
 
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { Box, Text, Tooltip } from "@chakra-ui/react";
+import { Box, Text, Tooltip, Button } from "@chakra-ui/react";
 import { COLORS } from "@/constants/theme";
+
+const MotionButton = motion(Button);
 
 const Action = () => {
   return (
     <Link href="#home" passHref>
-      <Box
-        as={motion.button}
-        px={3}
-        py={2}
-        justifyContent="center"
-        alignItems="center"
+      <MotionButton
+        as="a"
+        h="32px"
         whileHover={{ scale: 1.04 }}
         whileTap={{ scale: 0.9 }}
         bg={COLORS.btnGradient}
@@ -27,7 +26,7 @@ const Action = () => {
             Launch App
           </Text>
         </Tooltip>
-      </Box>
+      </MotionButton>
     </Link>
   );
 };
