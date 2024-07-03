05b92d0650fd9aa689bcf80ab1073e2f59780c9f
Sunday Ibrahim Ijai <ibrahimsundayijai@gmail.com>
2024-07-03 03:04:16 +0100

    chore: usebalances cache issues, added skeleton loaders

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -2,13 +2,6 @@
 import { ReactNode } from "react";
 import AppNavbar from "@/components/AppNavbar";
 import { AppFooter } from "@/components/AppFooter";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { SUBGRAPH } from "@/constants";
-
-const client = new ApolloClient({
-  uri: SUBGRAPH,
-  cache: new InMemoryCache(),
-});
 
 interface MainAppLayoutProps {
   children: ReactNode;
@@ -16,11 +9,11 @@ interface MainAppLayoutProps {
 
 const MainAppLayout: React.FC<MainAppLayoutProps> = ({ children }) => {
   return (
-    <ApolloProvider client={client}>
+    <>
       <AppNavbar />
       {children}
       <AppFooter />
-    </ApolloProvider>
+    </>
   );
 };
 
--- src/components/TokenSelector/token-select-list.tsx
+++ src/components/TokenSelector/token-select-list.tsx
@@ -1,18 +1,26 @@
+import React from "react";
 import { Box, VStack, Text, HStack } from "@chakra-ui/react";
 import TokenSelectListRow from "./token-select-row";
 import { useBalances } from "@/hooks/balances/useBalances";
 import { useAccount } from "wagmi";
 import { AssetClass } from "@/utils/classes";
+import { Skeleton, SkeletonCircle } from "@chakra-ui/react";
 
-function TokenSelectList({
-  userWalletTokens,
-}: {
+interface TokenSelectListProps {
   userWalletTokens: AssetClass[] | undefined;
-}) {
-  const { address } = useAccount();
+}
 
-  const xxx = useBalances({
-    account: address ?? "",
+interface ListContentProps {
+  address: string | undefined;
+  loading: boolean;
+  error: boolean;
+  walletBalance: AssetClass[] | undefined;
+}
+
+function TokenSelectList({ userWalletTokens }: TokenSelectListProps) {
+  const { address } = useAccount();
+  const { walletBalance, isError, isLoading } = useBalances({
+    account: address,
   });
 
   return (
@@ -23,7 +31,6 @@ function TokenSelectList({
       px="1rem"
       pb="1.4rem"
       overflowY="scroll"
-      // ref={vStackRef}
       gap="12px"
     >
       <HStack
@@ -35,15 +42,67 @@ function TokenSelectList({
       >
         <Text>Tokens</Text> <Text>Value</Text>
       </HStack>
-      {userWalletTokens?.map((token, i) => {
-        return (
-          <Box key={i} width="100%">
-            <TokenSelectListRow token={token} />
-          </Box>
-        );
-      })}
+      <ListContent
+        address={address}
+        loading={isLoading}
+        error={isError}
+        walletBalance={walletBalance}
+      />
     </VStack>
   );
 }
+
+const ListContent: React.FC<ListContentProps> = ({
+  address,
+  loading,
+  error,
+  walletBalance,
+}) => {
+  if (!address) {
+    return (
+      <Box>
+        <Text>No tokens to see here, Kindly Connect Wallet</Text>
+      </Box>
+    );
+  }
+
+  if (loading) {
+    return (
+      <>
+        {Array.from({ length: 8 }).map((_, i) => (
+          <RowSkeletonLoader key={i} />
+        ))}
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box>
+        <Text>An error occurred while fetching balance</Text>
+      </Box>
+    );
+  }
+
+  return (
+    <>
+      {walletBalance?.map((token, i) => (
+        <Box key={i} width="100%">
+          <TokenSelectListRow token={token} />
+        </Box>
+      ))}
+    </>
+  );
+};
+
+const RowSkeletonLoader: React.FC = () => (
+  <HStack width="100%" justifyContent="space-between">
+    <HStack>
+      <SkeletonCircle size="10" />
+      <Skeleton height="20px" width="80px" />
+    </HStack>
+    <Skeleton height="20px" width="60px" />
+  </HStack>
+);
 
 export default TokenSelectList;
