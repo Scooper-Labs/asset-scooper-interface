834e7ede1caebbaba26a13286fd5c0ae63f405cc
Sunday Ibrahim Ijai <ibrahimsundayijai@gmail.com>
2024-07-03 03:04:16 +0100

    chore: usebalances cache issues, added skeleton loaders

--- src/hooks/balances/useBalances.ts
+++ src/hooks/balances/useBalances.ts
@@ -2,33 +2,50 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../rtkHooks";
 import { RootState } from "@/store/store";
 import {
-  selectAllTokens,
   setUserWalletTokenWithBalance,
+  clearAllSelectedTokens,
 } from "@/store/sweep/sweepSlice";
 import { useWalletsPortfolio } from "../useMobula";
+import { useQuery } from "@tanstack/react-query";
+import { Address } from "viem";
+import { WalletPortfolioClass } from "@/utils/classes";
+import { AssetClass } from "@/utils/classes";
 
 interface UseBalances {
-  account: string;
+  account: Address | undefined;
 }
 export const useBalances = ({ account }: UseBalances) => {
   const dispatch = useAppDispatch();
 
-  const userWalletTokens = useAppSelector(
-    (state: RootState) => state.SweepTokensSlice.userWalletTokens
-  );
+  // const { data, error, loading } = useWalletsPortfolio();
 
-  const { data, error, loading } = useWalletsPortfolio();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["/walletBalance"],
+    queryFn: async () =>
+      fetch(`/api/portfolio?wallets=${account}`).then((response) =>
+        response.json(),
+      ),
+    // staleTime: ms("60s"),
+    // gcTime: ms("1h"),
+    refetchOnWindowFocus: true,
+    enabled: true,
+  });
 
   useEffect(() => {
     if (data) {
       try {
         dispatch(setUserWalletTokenWithBalance([]));
+        dispatch(clearAllSelectedTokens());
         dispatch(setUserWalletTokenWithBalance(data.assets));
       } catch (error) {
         console.error("Error validating data:", error);
       }
     }
-  }, [account]);
+  }, [data, isLoading]);
 
-  return { userWalletTokens, error, loading };
+  return {
+    walletBalance: data?.data?.assets as AssetClass[],
+    isError,
+    isLoading,
+  };
 };
