b841e3cbc0b9739089ce2da64e10429ed15609b5
dicethedev <dicethedev@gmail.com>
2024-07-04 20:50:18 +0100

    update...

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -3,9 +3,6 @@ import { ReactNode } from "react";
 import AppNavbar from "@/components/AppNavbar";
 import { AppFooter } from "@/components/AppFooter";
 import { Flex, Box } from "@chakra-ui/react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import { SUBGRAPH } from "@/constants";
-        
 interface MainAppLayoutProps {
   children: ReactNode;
 }
@@ -18,7 +15,7 @@ const MainAppLayout: React.FC<MainAppLayoutProps> = ({ children }) => {
         <Box flex="1">{children}</Box>
         <AppFooter />
       </Flex>
-   </>
+    </>
   );
 };
 
